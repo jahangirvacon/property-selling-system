3a2633b1637a957e409351af9a57253d420ef42f
shahid-haseeb <haseeb.shahid@emerald-labs.com>
2022-05-02 20:09:46 +0500

    ngrok updated

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,10 +1,10 @@
 // import { GURDWARA_API_BASE_URL } from "../config"
 // import store from "../store"
 
-const GURDWARA_API_BASE_URL = "https://5573-111-119-187-63.ngrok.io/v1"
+const GURDWARA_API_BASE_URL = "https://28fc-111-119-185-21.ngrok.io/v1"
 // const GURDWARA_API_BASE_URL = "http://localhost:4000/v1"
 
-const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjY1OGRlYjBlMDZjYTJlNjNkMjAzNGQiLCJ1c2VybmFtZSI6Imhhc2VlYkB0ZXN0LmNvbSIsImVtYWlsIjoiaGFzZWViQHRlc3QuY29tIiwicGhvbmVOdW1iZXIiOiIwIiwiaWF0IjoxNjUxNDYzMjU2LCJleHAiOjE2NTIxODMyNTZ9.No_vGrbqgC2d1Thncc2uqIiZ2Vt-EiNq0UG6C9IUk44'
+const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjY1OGRlYjBlMDZjYTJlNjNkMjAzNGQiLCJ1c2VybmFtZSI6Imhhc2VlYkB0ZXN0LmNvbSIsImVtYWlsIjoiaGFzZWViQHRlc3QuY29tIiwicGhvbmVOdW1iZXIiOiIwIiwiaWF0IjoxNjUxNTA0MTQzLCJleHAiOjE2NTIyMjQxNDN9.QHYhOWGEh-bpEzkdA7zDDAwvuqJvtnLsFa8pbV9bR_Y'
 
 export const post = async ({ url, baseURL, body, contentType, shouldAuthenticate }) => {
   const method = "POST"
