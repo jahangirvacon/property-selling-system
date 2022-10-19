966d60d37175a25c8bbed9ab6b4efb0e33522d89
bilawal35202 <bilawalhassan35202@gmail.com>
2022-10-19 15:46:47 +0500

    responsiveness

--- src/components/TopicMenu.js
+++ src/components/TopicMenu.js
@@ -1,33 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import {Menu, Row, Col} from "antd";
-import {DashboardOutlined} from "@ant-design/icons"
+import { Menu, Row, Col } from "antd";
+import { DashboardOutlined } from "@ant-design/icons"
 import './TopicMenu.css'
 
-const TopicMenu = ({ topics, selectedKey, changeSelectedKey }) => {
+const TopicMenu = ({ topics, selectedKey, changeSelectedKey, update }) => {
   const styledTopics = [];
   const getMenuItem = (topic) => {
-    if(topic.subMenu){
+    if (topic.subMenu) {
       return (
-        <Menu.SubMenu title={<div>{topic.icon}{topic.text}</div>}>
-          {topic.subMenu.map(option => 
-            <Menu.Item className="topics" key={option.text} onClick={changeSelectedKey}>
-              <Link to={option.link}>
-                {option.icon}{option.text}
-              </Link>
-            </Menu.Item>
-          )}
+        <Menu.SubMenu title={<div>{topic.icon}{update ? "" : topic.text}</div>}>
+            {topic.subMenu.map(option =>
+              <Menu.Item className="topics" key={option.text} onClick={changeSelectedKey}>
+                <Link to={option.link}>
+                  {option.icon}{update ? "" : option.text}
+                </Link>
+              </Menu.Item>
+            )}  
         </Menu.SubMenu>
       )
     } else {
       return (
         <Menu.Item className="topics" key={topic.text} onClick={changeSelectedKey}>
           <Link to={topic.link}>
-            {topic.icon}{topic.text}
+            {topic.icon}{update ? "" : topic.text}
           </Link>
         </Menu.Item>
       )
-      }
+    }
   }
   topics.forEach((topic, index) =>
     styledTopics.push(
